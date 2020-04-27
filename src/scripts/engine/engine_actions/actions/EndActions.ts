ee837dc6e06dde06c5f902303f31b83f2955e80d
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-27 19:17:15 +0200

    Add last card comparision

--- src/scripts/engine/engine_actions/actions/EndActions.ts
+++ src/scripts/engine/engine_actions/actions/EndActions.ts
@@ -10,6 +10,17 @@ const end_actions = {
 
     lastCardComparision: function () {
         console.log('Last Card Comparision')
+
+        this.won = true;
+
+        const winner = this.players.reduce((biggest, player) => { if( player.cards[0].value > biggest.value ){ return player.cards[0] } else { return biggest } } )
+        console.log(this.players)
+        console.log('winner: ')
+        console.log(winner);
+        this.players.forEach(player => {
+            player.id !== winner.id ? player.gameover = true : player
+        });
+        console.log(this.players)
     }
 }
 
